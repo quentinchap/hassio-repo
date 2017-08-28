53378138506b5c67cbadd220570ece428ad91390
quentinchap <chapelle.quentin@gmail.com>
2017-08-28 22:55:07 +0200

    update algo

--- tv-custom-api/route/channel.js
+++ tv-custom-api/route/channel.js
@@ -53,7 +53,7 @@ var getStdChaine = function(channel) {
 
         for (let str of channelTable) {
             lowerStr = str.toLowerCase();
-            if (lowerChannel == lowerStr) {
+            if (lowerStr.indexOf(lowerChannel) > -1) {
                 return c.stdValue;
             }
         }
